a2cbb40e5ec91d2e486bdfba01ba2246eb11ed59
janafrady <126286248+janafrady@users.noreply.github.com>
2026-05-03 20:33:45 -0500

    Update db.php

--- php/db.php
+++ php/db.php
@@ -2,11 +2,11 @@
 function getDB() {
     static $pdo = null;
     if ($pdo === null) {
-        $host = getenv('MYSQLHOST');
-        $db   = getenv('MYSQLDATABASE');
-        $user = getenv('MYSQLUSER');
-        $pass = getenv('MYSQLPASSWORD');
-        $port = getenv('MYSQLPORT');
+        $host = getenv('MYSQLHOST') ?: getenv('MYSQL_HOST');
+        $db   = getenv('MYSQLDATABASE' ?: getenv('MYSQL_DATABASE');
+        $user = getenv('MYSQLUSER') ?: getenv('MYSQL_USER');
+        $pass = getenv('MYSQLPASSWORD') ?: getenv('MYSQL_PASSWORD');
+        $port = getenv('MYSQLPORT') ?: getenv('MYSQSL_PORT');
         if (!$port) $port = '3306';
 
         $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
